44d6020c86737ef3131ab54aa0a39436c3dc14d4
DuvanGonzalez-ciclo <ciclocontinuo2@gmail.com>
2022-06-22 15:55:54 +0200

    src

---
+++ src/include/c_piscine.h
@@ -0,0 +1,26 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   c_piscine.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciclo-d <ciclo-d@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/22 15:16:17 by ciclo-d           #+#    #+#             */
+/*   Updated: 2022/06/22 15:16:19 by ciclo-d          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef C_PISCINE_H
+#define C_PISCINE_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+// c_00
+void	ft_putchar(char c);
+void	ft_print_alphabet(void);
+
+
+
+#endif
